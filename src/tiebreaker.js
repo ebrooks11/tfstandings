86c660c39d7d10dc5535432126631cb0ef66fe3b
Evan <evan.brooks88@gmail.com>
2018-08-26 15:44:10 -0500

    Removes comments from tiebreaker

--- src/tiebreaker.js
+++ src/tiebreaker.js
@@ -1,43 +1,22 @@
 import _ from 'lodash';
 
 export function breakMultiTeamTies(sortedTeams) {
-    console.log("BREAK MULTI TEAM TIES")
-    console.log("sortedTeams");
-    console.log(sortedTeams);
     var winGroups = _.groupBy(sortedTeams, (team) => {
-        // return team.tablePoints();
         return team.wins;
     });
 
-    console.log("winGroups");
-    console.log(winGroups);
-
-    // var map = _.map(sortedTeams, team => team.tablePoints());
-    var map = _.map(sortedTeams, team => team.wins);
-    console.log(map);
-    // var winGroupKeys = _.uniqBy(_.map(sortedTeams, team => team.tablePoints()), points => points);
     var winGroupKeys = _.uniqBy(_.map(sortedTeams, team => team.wins), points => points);
-
-    console.log("winGroupKeys");
-    console.log(winGroupKeys);
 
     var multiTeamTies = _.filter(winGroups, (group) => {
         return group.length > 2;
     });
 
-    console.log("multiTeamTies");
-    console.log(multiTeamTies);
-
     if (multiTeamTies.length === 0) return sortedTeams;
 
     multiTeamTies.forEach(winGroup => {
-        console.log("winGroup sent to breakMultiTeamTie");
-        console.log(winGroup);
         var sortedGroup = breakMultiTeamTie(winGroup);
 
         winGroups[winGroup[0].wins] = sortedGroup;
-
-        console.log(winGroups);
     });
 
     var fullySortedTeams = [];
@@ -52,25 +31,15 @@ export function breakMultiTeamTies(sortedTeams) {
 }
 
 export function breakMultiTeamTie(tiedTeams) {
-    console.log("BREAK MULTI TEAM TIE");
-    
     var sortedTeams = [];
 
     while(tiedTeams.length > 2){
         var tieWinner = getTieWinner(tiedTeams);
-        console.log("tieWinner");
-        console.log(tieWinner);
 
         sortedTeams.push(tieWinner);
 
         tiedTeams = _.without(tiedTeams, tieWinner);
-
-        console.log("tiedTeams");
-        console.log(tiedTeams);
     }
-
-    console.log("sortedTeams");
-    console.log(sortedTeams);
 
     if((tiedTeams[0].breakTie(tiedTeams[1]) > 0)){
         sortedTeams.push(tiedTeams[1]);
@@ -79,9 +48,6 @@ export function breakMultiTeamTie(tiedTeams) {
         sortedTeams.push(tiedTeams[0]);
         sortedTeams.push(tiedTeams[1]);
     }
-
-    console.log("sortedTeams");
-    console.log(sortedTeams);
 
     return sortedTeams;
 }
@@ -124,9 +90,6 @@ export function getTieWinner(tiedTeams){
             return teamsWithMaxWins[0];
         }
     
-        console.log("teamsWithMaxWins");
-        console.log(teamsWithMaxWins);
-
         return getTieWinner(teamsWithMaxWins);
     }
 
