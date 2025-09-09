1438705811e4e234c7816d57c1fc5d46328c3d35
Evan Brooks <evan.brooks88@gmail.com>
2025-09-09 09:25:07 -0400

    Adds 2025 to index

--- src/index.js
+++ src/index.js
@@ -229,23 +229,23 @@ class Standings extends React.Component {
                 }
             );
 
-        fetch('https://docs.google.com/spreadsheets/d/1sLyvQ862hHkdiEzd3p9XlDHrcsXONALO-7AEDjo83tw/gviz/tq?&sheet=2024&tq=' + query)
+        fetch('https://docs.google.com/spreadsheets/d/1sLyvQ862hHkdiEzd3p9XlDHrcsXONALO-7AEDjo83tw/gviz/tq?&sheet=2025&tq=' + query)
             .then(res => res.text())
             .then(rep => {
-                    var season = new Season(ScoringSystem.twoone, 2024, []);
+                    var season = new Season(ScoringSystem.twoone, 2025, []);
                     var data = JSON.parse(rep.substring(47).slice(0, -2));
                     data.table.rows.forEach(team => {
                         var scores = _.map(team.c.slice(2), function (score, index){
                             return score.v;
                         });
-                        season.teams.push(new Team(ScoringSystem.twoone, 2024, team.c[0].v, team.c[1].v, 0, scores));
+                        season.teams.push(new Team(ScoringSystem.twoone, 2025, team.c[0].v, team.c[1].v, 0, scores));
                     });
 
                     this.calculateTopFives(season);
 
                     seasons.push(season);
 
-                    var selectedYear = 2024;
+                    var selectedYear = 2025;
                     var selectedSeason = _.filter(seasons, season => season.year === selectedYear)[0];
                     this.setState( {value: selectedYear, season: selectedSeason});
                 }
@@ -253,7 +253,7 @@ class Standings extends React.Component {
 
         
 
-        var selectedYear = 2024;
+        var selectedYear = 2025;
         var selectedSeason = _.filter(seasons, season => season.year === selectedYear)[0];
         this.state = {value: selectedYear, season: selectedSeason || new Season()};
 
